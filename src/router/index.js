b851c1e7d70aec9044e9d60fc94e73af8185286d
YYB <512060925@qq.com>
2017-09-24 15:57:12 +0800

    static change

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import goods from '@/components/goods/goods'
-import ratings from '@/components/ratings/ratings'
-import seller from '@/components/seller/seller'
+import goods from '@/views/goods/goods'
+import ratings from '@/views/ratings/ratings'
+import seller from '@/views/seller/seller'
 
 Vue.use(Router)
 export default new Router({
+	mode:'history',
   routes: [
     {
       path: '/goods',
